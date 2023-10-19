2213dbd096a54e92b0f050092076b94cdee35b7b
AhmedNabil1994 <94ahmed.nabil@gmail.com>
2023-10-19 12:16:08 +0300

    landing page

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    // Back to top button
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 100, 'linear');
+        return false;
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1000,
+        center: true,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+
+    
+})(jQuery);
+
+// Translation popup
+document.getElementById("lang-button").addEventListener("click", ()=>{
+    document.querySelector(".lang-list").classList.toggle("show");
+})
+
+
+
+
+
+
