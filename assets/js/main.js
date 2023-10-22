ab66f5b6ef31fa65b0aa739a7eb09ac603eb5e0d
AhmedNabil1994 <94ahmed.nabil@gmail.com>
2023-10-22 14:29:25 +0300

    arrange the code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 (function ($) {
     "use strict";
-
     // Spinner
     var spinner = function () {
         setTimeout(function () {
@@ -12,7 +11,6 @@
     spinner();
     
     // Back to top button
-
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
             $('.back-to-top').fadeIn('slow');
@@ -53,11 +51,9 @@
 })(jQuery);
 
 window.addEventListener("load", function() {
-  let imageFullScreen = document.getElementById("lang-image");
-  let imageMobile = document.querySelector(".lang-image");
-  let imageElement = document.getElementById("lang-image");
-  let transTextMobile = document.querySelector(".trans-text");
-
+    let imageFullScreen = document.getElementById("lang-image");
+    let imageMobile = document.querySelector(".lang-image");
+    let transTextMobile = document.querySelector(".trans-text");
     if($('body').css('direction') == 'ltr'){
         imageFullScreen.src = "../assets/img/egypt.png"     
         imageMobile.src = "../assets/img/egypt.png"     
@@ -67,7 +63,6 @@ window.addEventListener("load", function() {
         imageMobile.src = "../assets/img/united-kingdom.png"     
         transTextMobile.textContent = "English"     
         }
-
 });
 
 
